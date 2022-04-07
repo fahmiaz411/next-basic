6164c1833ceab752b8757ff7558af3489e21b167
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 16:51:05 +0700

    server side props

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,46 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
+import styles from "../styles/Blog.module.css";
 
-function Blog() {
+type Blog = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+type BlogProps = {
+  dataBlog: Blog[];
+};
+
+function Blog(props: BlogProps) {
+  const { dataBlog } = props;
+
+  useEffect(() => {
+    console.log("fetch");
+  }, []);
+
   return (
     <Layout pageTitle="Blog">
-      <p>Blog Page</p>
+      {dataBlog.map((blog) => {
+        return (
+          <div key={blog.id} className={styles.card}>
+            <h3>{blog.title}</h3>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
 }
 
 export default Blog;
+
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const dataBlog = await res.json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
+}
