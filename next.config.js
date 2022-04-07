b68a6dd5ec873893622d4ae147101fcff3335587
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 11:55:29 +0700

    disable strict and fix twice render

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
