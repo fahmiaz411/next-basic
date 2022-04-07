bf7d4e97abb43334d6ce3d41a0e7ce3719bfe01c
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 12:19:53 +0700

    route params

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,13 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 
 function Detail() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <Layout pageTitle="Detail">
-      <div>Detail</div>
+      <div>Detail {id}</div>
     </Layout>
   );
 }
