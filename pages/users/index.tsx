ba73bfbbf4fdd521f2c493bed08502a33294e115
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 12:31:23 +0700

    getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,11 +1,29 @@
 import Layout from "../../components/Layout";
 
-function Users() {
+function Users({ dataUsers }: any) {
+  console.log(dataUsers);
   return (
     <Layout pageTitle="Users">
-      <div>Users</div>
+      {dataUsers.map((user: any) => {
+        return (
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </>
+        );
+      })}
     </Layout>
   );
 }
 
 export default Users;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
