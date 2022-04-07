855146904ca04490043cfbf563715638859f1000
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 12:17:00 +0700

    optimize image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,174 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout pageTitle="Home">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <Image src={"/KT.jpg"} width={200} height={200} />
+      <img src="/KT.jpg" style={{ width: 200, height: 200 }} alt="" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque earum
+        molestiae repellat, tenetur culpa laborum? Itaque, possimus sequi
+        deleniti minima magni, repellat facilis reprehenderit expedita
+        voluptatibus reiciendis commodi, voluptatem architecto.
+      </p>
       <h1 className={styles["title"]}>Welcome Fahmi</h1>
     </Layout>
   );
