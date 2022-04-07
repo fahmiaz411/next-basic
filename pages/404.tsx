ded1672da37387a3d406e678b516a701d6748ef2
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 11:55:38 +0700

    add auto redirect

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
+
   return (
     <div
       style={{
