e4b13913aff9d70e1f28a6e117efa51adecf1ca5
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-07 10:29:22 +0700

    add 404

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Oopps</h1>
+      <h1>Halaman tidak ditemukan</h1>
+    </div>
+  );
+}
