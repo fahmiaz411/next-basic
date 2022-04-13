e54f5e78bdd6bf37b8e72f70b365d96bb63c7900
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-13 18:32:43 +0700

    new hello

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,20 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout pageTitle="Home">
-      <div>Hello</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "80vh",
+          alignItems: "center",
+          justifyContent: "center",
+          fontFamily: "sans-serif",
+          fontWeight: "bold",
+          fontSize: 60,
+        }}
+      >
+        <p>Hello Fahmi</p>
+      </div>
     </Layout>
   );
 }
