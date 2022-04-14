8f8655d4fd44367ca68ec9a7f59125d1f83d2c4c
fahmiaz411 <fahmiaziz884@gmail.com>
2022-04-14 11:50:41 +0700

    remake header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,29 +1,54 @@
 // import Link from "next/link";
-import { CSSProperties } from "react";
-
-type Style = {
-  container: CSSProperties;
-};
-
-const styles: Style = {
-  container: {
-    width: "100%",
-    height: 50,
-    backgroundColor: "#0094fe",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-};
+import Image from "next/image";
+import styles from "./index.module.css";
 
 export default function Header() {
   return (
-    <header style={styles.container}>
-      <span>Left</span>
-      <span>Right</span>
+    <header className={styles.container}>
+      <span>
+        {/* Logo */}
+        <div className={styles["btn-pointer"]}>
+          <Image
+            src={require("../../public/kutech.svg")}
+            width={50}
+            height={50}
+          />
+        </div>
+        {/* Features */}
+        <span className={styles["feature-list"]}>
+          <ul>
+            <li>Flash Sale</li>
+            <li>Kelas</li>
+            <li>Bootcamp</li>
+            <li>Resources</li>
+          </ul>
+        </span>
+      </span>
+      <span>
+        {/* Search */}
+        <div className={styles["search"] + " " + styles["btn-pointer"]}>
+          <Image
+            src={require("../../public/search.svg")}
+            width={25}
+            height={25}
+            style={{
+              borderRadius: "50%",
+            }}
+          />
+        </div>
+        <span className={styles["margin-h-sm"]}>Halo, Fahmi</span>
+        {/* Profile Pic */}
+        <div className={styles["btn-pointer"]}>
+          <Image
+            src={require("../../public/unnamed.jpg")}
+            width={50}
+            height={50}
+            style={{
+              borderRadius: "50%",
+            }}
+          />
+        </div>
+      </span>
     </header>
   );
 }
