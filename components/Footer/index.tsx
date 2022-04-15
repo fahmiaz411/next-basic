0240c5487ffff12a6d7d81650639e1b6c70d8ed8
Fahmi Aziz <fahmiaziz884@gmail.com>
2022-04-15 09:40:43 +0700

    @copyright

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div>
-      <p className={styles.title}>Made with love @fahmi</p>
+      <p className={styles.title}>Copyright © 2022 | Kursus Teknologi</p>
     </div>
   );
 }
